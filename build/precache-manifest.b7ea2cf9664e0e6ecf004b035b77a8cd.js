db7bf68c3bde78e46a5403ac8a172c468ff55b4c
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2020-10-22 10:59:54 +0100

    The build of the app
    I didn't know that the build of the app was previously ignored

---
+++ build/precache-manifest.b7ea2cf9664e0e6ecf004b035b77a8cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c61ae1b1ae1e58d5079748d3d42736",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab1d32e952dcb35d4123",
+    "url": "./static/css/main.e21ba4ec.chunk.css"
+  },
+  {
+    "revision": "6e5a795289dd2d53dfd4",
+    "url": "./static/js/2.b15e5bf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b15e5bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1d32e952dcb35d4123",
+    "url": "./static/js/main.52aed464.chunk.js"
+  },
+  {
+    "revision": "066e1fe302588d5b0de2",
+    "url": "./static/js/runtime-main.560b22e6.js"
+  }
+]);
